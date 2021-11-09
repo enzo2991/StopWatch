86cf3b4efba9827350a41c08ab1ccd64995019b9
Enzo Galantino <enzo2991@live.fr>
2021-11-09 20:33:42 +0100

    Add files via upload

--- cogs.py
+++ cogs.py
@@ -6,6 +6,15 @@ import datetime
 import function
 import asyncio
 
+timereturn = "%d/%m/%Y %H:%M"
+datereturn = "%d/%m/%Y %H:%M:%S"
+
+def log(ctx,start_time,end_time,duree):
+    logs = "|" + str(start_time.strftime(timereturn) + "|\n" + f'Auteur : {ctx.author}\n Fin du spec: {end_time.strftime(timereturn)}\n Durée: {duree}\n__________________________________________\n') 
+    with open('logs.txt','a') as f:
+        f.write(logs)
+    return log
+
 class spec(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
@@ -18,7 +27,7 @@ class spec(commands.Cog):
         def check(reaction, user):
             return user == ctx.author and (str(reaction.emoji) in "❌")
         try:
-            reaction, user = await self.bot.wait_for('reaction_add', timeout=64800 , check=check) 
+            reaction, user = await self.bot.wait_for('reaction_add', timeout=64800 , check=check)
         except asyncio.TimeoutError:
             await ctx.message.delete()
             await ctx.send(f'<@{ctx.author.id}>, tu dors ?', delete_after=20)
@@ -27,9 +36,10 @@ class spec(commands.Cog):
             duration  = end_time - start_time
             duration_in_s = duration.total_seconds()
             convert = function.convert_seconds(duration_in_s)
+            log(ctx,start_time,end_time,convert)
             await ctx.message.delete()
             info = discord.Embed(title=user, color=0x318bdb)
-            info.add_field(name="Debut du spec:",value=start_time)
-            info.add_field(name="Fin du spec:",value=end_time)
+            info.add_field(name="Debut du spec:",value=start_time.strftime(datereturn))
+            info.add_field(name="Fin du spec:",value=end_time.strftime(datereturn))
             info.add_field(name="Durée:",value=convert)
             await ctx.send(embed=info)
