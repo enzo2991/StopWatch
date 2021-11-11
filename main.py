db22a97cfec125639db40cf036ee826d2ff7cdf6
Enzo Galantino <enzo2991@live.fr>
2021-11-11 13:28:28 +0100

    ajout de la changed_presence

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from discord import activity
 from discord.ext import commands
 from dotenv import load_dotenv
 from os import getenv
 import cogs
+import discord
 
 load_dotenv()
 
@@ -10,6 +12,8 @@ bot = commands.Bot(command_prefix=".")
 @bot.event
 async def on_ready():
      print("Ready !")
+     activiter = discord.Streaming(name="KellOog'S RP", url="https://twitch.tv/Weetoos")
+     await bot.change_presence(activity=activiter)
      
 
 bot.add_cog(cogs.spec(bot))
